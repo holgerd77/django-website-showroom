6d8ec2ee6faff3879bcc358fa5461bab6fceb8f2
holgerd77 <Holger.Drewes@googlemail.com>
2012-06-29 15:58:38 +0200

    Changed image save behaviour to avoid problems when changing image

--- website_showroom/models.py
+++ website_showroom/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 import os.path
 
@@ -16,6 +18,13 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
+
+def get_path(instance, filename):
+    pos = filename.rfind('.')
+    path = 'screenshots/' + 's_' + str(uuid.uuid1()) + filename[pos:]
+    return path
+
+
 class Website(models.Model):
     title = models.CharField(max_length=50)
     category = models.ForeignKey(Category)
@@ -24,8 +33,8 @@ class Website(models.Model):
     country = models.CharField(max_length=2, null=True, blank=True, help_text=help_text)
     help_text = "Tip: Keep it short, space is limited!"
     desc = models.TextField(help_text=help_text)
-    help_text = "Image file, size: 300x200, name will be normalized to 'website_x.xxx'. If you provide a larger file image will be resized (use same proportions, e.g. 600x400 or 750x500)."
-    screenshot = models.ImageField(upload_to='screenshots', help_text=help_text)
+    help_text = "Image file, size: 300x200, name will be unified. If you provide a larger file image will be resized (use same proportions, e.g. 600x400 or 750x500)."
+    screenshot = models.ImageField(upload_to=get_path, help_text=help_text)
     url = models.CharField(max_length=90)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
@@ -34,31 +43,24 @@ class Website(models.Model):
     def __unicode__(self):
         return self.title
 
-    def save(self):
-        try:
-            this = Website.objects.get(id=self.id)
-            if this.screenshot != self.screenshot:
-                this.screenshot.delete()
-        except: pass
-        
-        super(Website, self).save()
-        
-        from PIL import Image
-        filename, ext = os.path.splitext(self.screenshot.name)
-        image = Image.open(self.screenshot)
-        image.thumbnail([300, 200], Image.ANTIALIAS)
-        new_filename = 'screenshots/website_' + str(self.id) + ext
-        image.save(settings.MEDIA_ROOT + '/' + new_filename)
-        if(new_filename != self.screenshot.name):
-            os.remove(settings.MEDIA_ROOT + '/' + self.screenshot.name)
-        self.screenshot = new_filename
-        super(Website, self).save()
+
+@receiver(post_save, sender=Website)
+def post_save_handler(sender, instance, using, **kwargs):
+    from PIL import Image
+    image = Image.open(instance.screenshot)
+    print "test"
+    print image
+    image.thumbnail([300, 200], Image.ANTIALIAS)
+    image.save(settings.MEDIA_ROOT + '/' + instance.screenshot.name)
+    
 
 @receiver(pre_delete, sender=Website)
 def pre_delete_handler(sender, instance, using, **kwargs):
     try:
-        os.remove(settings.MEDIA_ROOT + '/' + instance.screenshot.name)
+        path = settings.MEDIA_ROOT + '/' + instance.screenshot.name
+        os.remove(path)
     except OSError:
         pass
 
 pre_delete.connect(pre_delete_handler, sender=Website)
+post_save.connect(post_save_handler, sender=Website)
