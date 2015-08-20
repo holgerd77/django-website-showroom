1dd025963f960ea8dd04dfd039234eb082ad7a6f
holgerd77 <Holger.Drewes@googlemail.com>
2015-08-20 12:59:54 +0200

    Fixed Haystack search in search_indexes.py file

--- website_showroom/search_indexes.py
+++ website_showroom/search_indexes.py
@@ -1,7 +1,7 @@
 from haystack import indexes
 from website_showroom.models import EditionWebsite
 
-class EditionWebsiteIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
+class EditionWebsiteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
