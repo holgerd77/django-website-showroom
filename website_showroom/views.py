da963ede9e77c89bd29abfaf37fff6000ecb2a73
holgerd77 <Holger.Drewes@googlemail.com>
2015-08-25 12:09:10 +0200

    Fixed bug when using same category URL name for different editions

--- website_showroom/views.py
+++ website_showroom/views.py
@@ -88,7 +88,7 @@ def category(request, ed_country, url_name):
     ed_category_list = get_ed_category_list(act_edition)
     home_dummy_ed_cat = get_home_dummy_ed_cat(act_edition)
     ed_category_list.insert(0, home_dummy_ed_cat)
-    act_ed_cat = get_object_or_404(EditionCategory, url_name=url_name)
+    act_ed_cat = get_object_or_404(EditionCategory, edition=act_edition, url_name=url_name)
     ed_website_list = EditionWebsite.objects.filter(edition=act_edition, website__category=act_ed_cat.category).order_by('order')
     c = RequestContext(request, {
         'edition_list': get_edition_list(),
